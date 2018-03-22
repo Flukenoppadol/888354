1bcb4de2b35594a779a080318ef7410eec464d25
Noppadol <58160238@go.buu.ac.th>
2018-03-22 15:01:21 +0700

    commit 22/3/2561

---
+++ nodejs/module_path.js
@@ -0,0 +1,13 @@
+var path = require('path');
+var filename = path.basename('workshop/node_js/basic.js');
+var current_filename = path.basename(__filename);
+console.log("filename : "+ filename);
+console.log("current_path : "+ __filename);
+console.log("current_filename : "+ current_filename);
+var dir = path.dirname('workshop/node_js/basic.js');
+var current_dir = path.dirname(__filename);
+console.log("dir : "+dir);
+console.log("current_dir : "+current_dir);
+console.log("current_dir 2 : "+ __dirname);
+var join_path = path.join('workshop','node_js','basic.js');
+console.log("join_path : "+ join_path);
---
+++ nodejs/module_timers.js
@@ -0,0 +1,6 @@
+var myinterval = setInterval(function(){
+    console.log("Hello myTimeInterval");
+}, 1000);
+var myTimeout = setTimeout(function(){
+    console.log("Hello myTimeOut");
+}, 3000);
